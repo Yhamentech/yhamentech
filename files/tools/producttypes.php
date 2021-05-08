7f28c3da4e419793847e60ae6e55843704995cbd
Yhamentech <82400849+Yhamentech@users.noreply.github.com>
2021-05-08 17:04:48 +0800

    Add files via upload

---
+++ files/tools/producttypes.php
@@ -0,0 +1,30 @@
+<?php
+$productNames = array(
+	
+    'iPhone8,1' => 'iPhone 6s (GSM)',
+    'iPhone8,1' => 'iPhone 6s (Global)',
+	'iPhone9,1' => 'iPhone 7  (GSM)',
+	'iPhone9,1' => 'iPhone 7  (17G68)',
+	'iPhone9,1' => 'iPhone 7  (17H35)',
+	'iPhone9,1' => 'iPhone 7  (18D70)',
+	'iPhone9,2' => 'iPhone 7+  (GSM)',
+	'iPhone9,2' => 'iPhone 7+  (17G68)',
+	'iPhone9,2' => 'iPhone 7+  (17H35)',
+	'iPhone9,2' => 'iPhone 7+  (18D70)',
+	'iPhone10,1' => 'iPhone 8  (GSM)',
+	'iPhone10,1' => 'iPhone 8  (17G68)',
+	'iPhone10,1' => 'iPhone 8  (17H35)',
+	'iPhone10,1' => 'iPhone 8  (18D70)',
+	'iPhone10,2' => 'iPhone 8+ (GSM)',
+	'iPhone10,2' => 'iPhone 8+ (17G68)',
+	'iPhone10,2' => 'iPhone 8+ (17H35)',
+	'iPhone10,2' => 'iPhone 8+ (18D70)',
+	'iPhone10,3' => 'iPhone x (GSM)',
+	'iPhone10,3' => 'iPhone x (17G68)',
+	'iPhone10,3' => 'iPhone x (17H35)',
+	'iPhone10,3' => 'iPhone x (18D70)',
+	
+	
+	
+	
+?>
